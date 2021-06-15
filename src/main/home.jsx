23b2ef35836c6c4f7c0641cf36e2820d66d04e51
parth <dankparth@gmail.com>
2021-06-15 09:14:57 +0530

    completed

--- src/main/home.jsx
+++ src/main/home.jsx
@@ -1,9 +1,94 @@
 import React from 'react'
-
-export default function home() {
+import './home.css'
+import image from './instagram.png'
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import firebaseConfig from '../firebase';
+import firebase from 'firebase';
+import { v4 as uuidv4 } from 'uuid';
+import {useState,useEffect,useRef} from 'react'
+import {useHistory} from 'react-router-dom'
+import ClipLoader from "react-spinners/ClipLoader";
+import BounceLoader from 'react-spinners/BounceLoader';
+export default function Home() {
+  const [images,addImages] = useState([])
+  const div_ref = useRef()
+  const size_ref = useRef()
+  const caption = useRef()
+  const history = useHistory();
+  const [isLoading,setLoading] = useState(true);
+  var file = ''
+    useEffect(()=>{
+      const div = div_ref.current
+      
+      firebase.database().ref().on('value',(snapshot,err)=>{
+        while(div.firstChild){
+          div.removeChild(div.firstChild)
+        }
+        console.log(snapshot.val())
+        snapshot.forEach(items=>{
+          console.log(items.val().url,items.val().name) 
+          addImages(prev=>[...prev,{name:items.val().name,url:items.val().url,caption:items.val().caption}])
+        })
+        
+      })
+    },[])
     return (
         <div>
-          hello my name is parth sharma  
+          <nav>
+          <img src={image} draggable='false' alt="" />
+          <ExitToAppIcon className='icon' style={{fontSize:'50px'}} onClick={()=>{
+            firebase.auth().signOut()
+            localStorage.removeItem('email')
+            localStorage.removeItem('password')
+            history.push('/')
+          }}/>
+          </nav>
+          <h1> {firebase.auth().currentUser.email} </h1>
+          <div>
+         <div ref={div_ref} className="🚀" >
+           <BounceLoader color='blue' loading={isLoading} size={150} />
+           {
+             images.map(items=>{
+               return(
+                 <div>
+                   <span>
+                    <p  className='rounded'> {items.name[0]} </p>
+                   <p> {items.name} </p>
+                   </span>
+                   <img src={items.url} alt="" />
+                   
+                   <p> <strong>{items.name}</strong> {items.caption} </p>
+                 </div>
+               )
+              })
+           }
+         </div>
+         <input type="file" onChange={(e)=>{
+             file = e.target.files[0]
+            }} />
+           <button onClick={()=>{
+             console.log(firebase.auth().currentUser)
+             var name = uuidv4();
+                console.log(name)
+                var path = firebase.storage().ref().child(name)
+                console.log(firebase.auth().currentUser.displayName)
+               file.length!=0 && caption.current.value.length!=0 ? path.put(file).then(()=>{
+                 path.getDownloadURL().then((url)=>{
+                    console.log(url)
+                    firebase.database().ref().child(name).set({
+                      'name':firebase.auth().currentUser.email,
+                      'url':url,
+                      'caption':caption.current.value
+                    })
+                  })
+                }):
+                alert('please select a file')
+              }}>
+             Upload
+           </button>
+           <br />
+           <input type="text" ref={caption} />
+        </div>
         </div>
     )
 }
--- src/sign_up/sign_up.jsx
+++ src/sign_up/sign_up.jsx
@@ -2,21 +2,38 @@ import React from 'react'
 import './sign_up.css'
 import firebase from 'firebase'
 import firebaseConfig from '../firebase'
-import {useRef} from 'react'
+import {useRef,useState,useEffect} from 'react'
 import { useHistory } from 'react-router-dom'
 import { TextField } from '@material-ui/core'
+import ClipLoader from "react-spinners/ClipLoader";
+import { BarLoader } from 'react-spinners'
 
 export default function Sign_up() {
     const emailRef = useRef();
     const passwordRef = useRef();
+    const userNameRef = useRef();
     const history = useHistory();
-
+    const [isRotating,setRotating] = useState(false)
    
-    function signUpUser(){
 
-      emailRef.current.value.length!=0 && passwordRef.current.value.length!=0 ? firebase.auth().createUserWithEmailAndPassword(emailRef.current.value,passwordRef.current.value)
+    useEffect(()=>{
+        if(localStorage.getItem('email') && localStorage.getItem('password') != null){
+                firebase.auth().signInWithEmailAndPassword(localStorage.getItem('email'),localStorage.getItem('password'))
+                .then(()=>{
+                    history.push('/Home')
+                })
+        }
+        
+    })
+
+    function signUpUser(){
+      setRotating(true)
+      emailRef.current.value.length!=0 && passwordRef.current.value.length!=0 && userNameRef.current.value!=0 ? firebase.auth().createUserWithEmailAndPassword(emailRef.current.value,passwordRef.current.value)
         .then(user=>{
+          setRotating(false)  
             console.log(user.user.email)
+            localStorage.setItem('email',user.user.email)
+            localStorage.setItem('password',passwordRef.current.value)
             history.push('/Home')
         })
         :
@@ -24,18 +41,23 @@ export default function Sign_up() {
     }
     return (
         <div>
+            <h1>Create An Account</h1>
             <div className='main'>
                 <img src="https://post.healthline.com/wp-content/uploads/2018/12/How-to-Be-Happy_1200x628-facebook.jpg" alt="" draggable='false' />
                 <form action="">
+               
                 <div>
-                <input type="email" ref={emailRef}/>
+                <input type="email" ref={emailRef} placeholder='Enter Your Email'/>
                 <br /><br />
-                <input type="password" ref={passwordRef} />
+                <input type="password" ref={passwordRef} placeholder='Enter Your Password' />
+                <br /><br />
+                <input type="text" placeholder='Enter Your Username' ref={userNameRef} />
                 <br /><br />
                 <input type="button" value="Sign Up" onClick={()=>signUpUser()} />
                 </div>
                 </form>
             </div>     
+                <BarLoader color='blue' width={100} height={4} loading={isRotating}/>
         </div>
     )
 }
